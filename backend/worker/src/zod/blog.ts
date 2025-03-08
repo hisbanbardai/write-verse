9d7a284694cd194cc1b9859a19d972dc422b5424
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-08 12:52:44 -0700

    Add zod validation schema for updating a blog

--- backend/worker/src/zod/blog.ts
+++ backend/worker/src/zod/blog.ts
@@ -1,8 +1,18 @@
 import { z } from "zod";
 
 export const createBlogSchema = z.object({
-  title: z.string(),
-  content: z.string(),
+  title: z.string().min(6, "Title must be of at least 6 characters"),
+  content: z.string().min(6, "Content must be of at least 6 characters"),
 });
 
 export type createBlogSchemaT = z.infer<typeof createBlogSchema>;
+
+export const updateBlogSchema = z.object({
+  title: z.string().min(6, "Title must be of at least 6 characters").optional(),
+  content: z
+    .string()
+    .min(6, "Content must be of at least 6 characters")
+    .optional(),
+});
+
+export type updateBlogSchemaT = z.infer<typeof updateBlogSchema>;
