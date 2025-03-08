b473fedc2e66f75f7f220c6d812d491ddba2b250
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-07 18:47:23 -0700

    Add utility function to verify hashed password

--- backend/worker/src/utils/password.ts
+++ backend/worker/src/utils/password.ts
@@ -15,3 +15,17 @@ export const hashPassword = async function (body: {
 
   return await response.json();
 };
+
+export const verifyPassword = async function (body: {
+  password: string;
+  hashedPassword: string;
+}): Promise<{ isValid: boolean }> {
+  const response = await fetch("http://localhost:3001/verify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  return await response.json();
+};
