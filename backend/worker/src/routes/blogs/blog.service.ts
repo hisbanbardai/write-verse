48445449102c67c697f0bf9257826e810b452aea
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-08 12:53:52 -0700

    Add getBlogById and updateBlog methods in BlogService class

--- backend/worker/src/routes/blogs/blog.service.ts
+++ backend/worker/src/routes/blogs/blog.service.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { createBlogSchemaT } from "../../zod/blog";
+import { createBlogSchemaT, updateBlogSchemaT } from "../../zod/blog";
 
 export class BlogService {
   private prisma;
@@ -34,6 +34,40 @@ export class BlogService {
       return createdBlog;
     } catch (error) {
       console.error("Error creating blog", error);
+      throw error;
+    }
+  }
+
+  async updateBlog(data: updateBlogSchemaT, blogId: number, userId: number) {
+    try {
+      const updatedBlog = await this.prisma.blogs.update({
+        data: {
+          title: data.title,
+          content: data.content,
+        },
+        where: {
+          id: blogId,
+          authorId: userId,
+        },
+      });
+
+      return updatedBlog;
+    } catch (error) {
+      console.error("Error updating blog:", error);
+      throw error;
+    }
+  }
+
+  async getBlogById(blogId: number) {
+    try {
+      const existingBlog = await this.prisma.blogs.findUnique({
+        where: {
+          id: blogId,
+        },
+      });
+      return existingBlog;
+    } catch (error) {
+      console.error("Error fetching the blog by id", error);
       throw error;
     }
   }
