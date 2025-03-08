c2c990937ca5c8002ac46d52f866f334fcfdad97
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-08 11:15:37 -0700

    Implement getAllBlogs route

--- backend/worker/src/routes/blogs/index.ts
+++ backend/worker/src/routes/blogs/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from "hono";
+import { BlogService } from "./blog.service";
 
-export const blogsRouter = new Hono();
+export const blogsRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
 blogsRouter.get("/bulk", async (c) => {
-  return c.text("Hello all blogs");
+  try {
+    const blogService = new BlogService(c.env.DATABASE_URL);
+    const blogs = await blogService.getAllBlogs();
+
+    return c.json({ blogs }, 200);
+  } catch (error) {
+    console.error("Failed to fetch the blogs", error);
+    return c.json({ message: "Failed to fetch the blogs" }, 500);
+  }
 });
 
 blogsRouter.post("/", async (c) => {
