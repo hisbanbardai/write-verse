ded01593d3bbf6641ff20d8beca7e93e86d4b8f9
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-13 12:45:10 -0600

    Add initial routing to pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ProtectedRoutes } from "./components/auth/ProtectedRoutes";
+import { PublicRoutes } from "./components/auth/PublicRoutes";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Blogs } from "./pages/Blogs";
+import { Blog } from "./pages/Blog";
+import { BlogCreate } from "./pages/BlogCreate";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PublicRoutes />}>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+          </Route>
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/blog/:id" element={<Blog />} />
+            <Route path="/blog/new" element={<BlogCreate />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
