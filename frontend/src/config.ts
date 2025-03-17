e36ac52b7581fb423273d78080f934e14e214ca3
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-17 14:52:20 -0600

    Remove previous config data and add backend url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,2 @@
-export const SIGNIN_API_URL =
-  "https://write-verse.hisbanbardai.workers.dev/api/v1/users/signin";
-export const SIGNUP_API_URL =
-  "https://write-verse.hisbanbardai.workers.dev/api/v1/users/signup";
+export const BACKEND_URL =
+  "https://write-verse.hisbanbardai.workers.dev/api/v1";
