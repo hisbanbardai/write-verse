704d2fba85ad3b9b238a18c7423756647862a03a
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-18 17:24:53 -0600

    Create a useBlogs custom hook and move all the blogs fetching logic. Add useQuery to cache the results

---
+++ frontend/src/hooks/useBlogs.ts
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../config";
+import { TBlogCardProps } from "../components/blog/BlogCard";
+import { useQuery } from "@tanstack/react-query";
+
+type TBlogAuthor = {
+  firstName: string;
+  lastName: string;
+};
+
+type TBlog = TBlogCardProps & {
+  id: string;
+  author: TBlogAuthor;
+};
+
+type TBlogResponse = {
+  blogs: TBlog[];
+  totalPages: number;
+};
+
+export const useBlogs = function () {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  function handlePageChange(direction: string) {
+    if (direction === "previous") {
+      setCurrentPage((prev) => prev - 1);
+    }
+
+    if (direction === "next") {
+      setCurrentPage((prev) => prev + 1);
+    }
+  }
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [currentPage]);
+
+  async function fetchAllBlogs(): Promise<TBlogResponse> {
+    const response = await axios.get(`${BACKEND_URL}/blogs/bulk`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      params: {
+        page: currentPage,
+        pageSize: 5,
+      },
+    });
+
+    return response.data;
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["page-number", currentPage],
+    queryFn: () => fetchAllBlogs(),
+    staleTime: 1000 * 60 * 60,
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
+
+  const blogs = data?.blogs || [];
+  const totalNumOfPages = data?.totalPages || 1;
+
+  return {
+    isLoading,
+    blogs,
+    currentPage,
+    handlePageChange,
+    totalNumOfPages,
+  };
+};
