7e5a32b73d3f4ecbc3afa001d3d4a231cbfc404c
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-19 13:05:37 -0600

    Add a route to validate token

--- backend/worker/src/routes/root/index.ts
+++ backend/worker/src/routes/root/index.ts
@@ -1,8 +1,16 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { blogsRouter } from "../blogs";
 import { usersRouter } from "../users";
+import { authMiddleware } from "../../middlewares/auth";
 
 export const rootRouter = new Hono();
 
 rootRouter.route("/users", usersRouter);
 rootRouter.route("/blogs", blogsRouter);
+
+rootRouter.get("/auth/validate-token", authMiddleware, (c: Context) => {
+  if (c.get("userId")) {
+    return c.json({ valid: true }, 200);
+  }
+  return c.json({ valid: false }, 401);
+});
