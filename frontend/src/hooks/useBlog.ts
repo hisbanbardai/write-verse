562225997c3ef56aa99f657a4bf68ddc6c3af2c8
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-20 17:09:59 -0600

    Create a custom hook to fetch specific blog details

---
+++ frontend/src/hooks/useBlog.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+
+export function useBlog(id: string | undefined) {
+  const isIdValid = id && !isNaN(Number(id));
+
+  async function fetchBlog() {
+    const response = await axios.get(`${BACKEND_URL}/blogs/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return response.data;
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["blogId", id],
+    queryFn: fetchBlog,
+    staleTime: 1000 * 60 * 60,
+    refetchOnWindowFocus: false,
+    retry: false,
+    enabled: Boolean(isIdValid),
+  });
+
+  const blog = data?.existingBlog || null;
+
+  return { blog, isLoading };
+}
