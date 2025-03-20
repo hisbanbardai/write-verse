a5efac413b8a7af0f57f9d3e180d29861a319adc
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-20 12:47:54 -0600

    Render components according to the state of authentication

--- frontend/src/components/auth/PublicRoutes.tsx
+++ frontend/src/components/auth/PublicRoutes.tsx
@@ -1,5 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthentication } from "../../hooks/useAuthentication";
+import { Loader } from "../common/Loader";
 
 export const PublicRoutes = function () {
-  return <Outlet />;
+  const { isAuthenticated } = useAuthentication();
+
+  if (isAuthenticated === null) {
+    return <Loader />;
+  }
+
+  return !isAuthenticated ? <Outlet /> : <Navigate to={"/blogs"} replace />;
 };
