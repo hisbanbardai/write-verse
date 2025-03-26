4727fe93d05fe1c8378c83e1b9f567f2b128a714
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-26 14:30:30 -0600

    Minor changes in blog schema

--- common/src/zod/blog.ts
+++ common/src/zod/blog.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
 
 export const createBlogSchema = z.object({
-  title: z.string().min(6, "Title must be of at least 6 characters"),
-  content: z.string().min(6, "Content must be of at least 6 characters"),
+  title: z
+    .string()
+    .min(6, "Title must be at least 6 characters")
+    .max(255, "Title is too long"), // Limit to a reasonable length
+  content: z.string().min(6, "Content must be at least 6 characters"),
 });
 
 export type createBlogSchemaT = z.infer<typeof createBlogSchema>;
 
 export const updateBlogSchema = z.object({
-  title: z.string().min(6, "Title must be of at least 6 characters").optional(),
-  content: z
-    .string()
-    .min(6, "Content must be of at least 6 characters")
-    .optional(),
+  title: z.string().min(6).max(255).optional(),
+  content: z.string().min(6).optional(),
 });
 
 export type updateBlogSchemaT = z.infer<typeof updateBlogSchema>;
