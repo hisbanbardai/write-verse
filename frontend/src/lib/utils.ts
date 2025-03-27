d40bc58fe074b766f552d70c619e7b3b2bb9d604
Hisban Shiraz <hisbanbardai@gmail.com>
2025-03-27 17:04:39 -0600

    Update handleError utility logic if the error is an object and not just a string

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -6,10 +6,13 @@ export const handleError = function (error: unknown) {
 
   if (error instanceof AxiosError) {
     message = error.response?.data.message;
-    for (const key of Object.keys(message)) {
-      toast.error(message[key][0]);
+
+    if (typeof message !== "string") {
+      for (const key of Object.keys(message)) {
+        toast.error(message[key][0]);
+      }
+      return;
     }
-    return;
   } else if (error instanceof Error) {
     message = error.message;
   } else {
